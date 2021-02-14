86e90760bc8d743a0e9e1ee0c918c3679a9d331f
stmer1 <33815390+stmer1@users.noreply.github.com>
2021-02-13 16:23:17 -0800

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 15:27:44 2021
+
+@author: jmami
+"""
+
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+st.title('My first app')
